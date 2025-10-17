eecf549ecba0107bbff874913f827e901d0d3cbf
akashchy23 <akashchypuc2@gmail.com>
2025-10-17 18:49:01 +0600

    added all apps using searchbar

--- src/Pages/Apps.jsx
+++ src/Pages/Apps.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import search from '../assets/search.png'
+import useAppsData from '../hooks/useApps';
+import AppsCard from './AppsCard';
 const Apps = () => {
+    const { apps } = useAppsData()
+    const [searchApps, setSearchApps] = useState('');
+    const term = searchApps.trim().toLowerCase()
+    const searchedApps = term ?
+        apps.filter(app => app.title.toLowerCase().includes(term))
+        : apps;
+    console.log(searchedApps)
     return (
-        <div>
-            <p>here will be apps</p>
+        <div className='max-w-[1440px] mx-auto'>
+            <div className='text-center mt-10 md:mt-15 space-y-4'>
+                <h1 className='font-bold text-5xl'>Our All Applications</h1>
+                <p className='text-[#627382]'>Explore All Apps on the Market developed by us. We code for Millions</p>
+            </div>
+
+            <div>
+                <div className='flex flex-col space-y-5 md:flex-row md:justify-between items-center mb-4'>
+                    <p className='font-semibold text-2xl'>({searchedApps.length}) Apps Found </p>
+                    <label className='input'>
+                        <img src={search} alt="" />
+                        <input value={searchApps} onChange={(e) => setSearchApps(e.target.value)} type="search" placeholder='search apps here' />
+                    </label>
+                </div>
+                <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full mx-auto mb-15'>
+                    {
+                        searchedApps.map(app => <AppsCard app={app}></AppsCard>)
+                    }
+                </div>
+            </div>
         </div>
+
     );
 };
 
--- src/Pages/AppsCard.jsx
+++ src/Pages/AppsCard.jsx
@@ -4,19 +4,20 @@ import downloadImg from '../assets/icon-downloads.png'
 import rateIcon from '../assets/icon-ratings.png'
 
 const AppsCard = ({ app }) => {
-    const {image} = app;
+    const {image ,title,downloads,reviews
+} = app;
     return (
         <div className='bg-[#FFFFFF] w-87 mx-auto shadow-sm p-4 rounded-xl'>
             <img className='h-[316px] w-[316px]' src={image} alt="" />
-            <h1 className='font-medium my-2'>Forest: Focus for Productivity</h1>
+            <h1 className='font-medium my-2'>{title}</h1>
             <div className='flex justify-between my-2'>
                 <button className='bg-[#F1F5E8] flex items-center p-2 rounded-xs'>
                     <img className='w-3 h-3' src={downloadImg} alt="" />
-                    <p className='text-[#00D390] font-medium ml-2'>9M</p>
+                    <p className='text-[#00D390] font-medium ml-2'>{downloads}</p>
                 </button>
                 <button className='bg-[#F1F5E8] flex items-center p-2 rounded-xs'>
                     <img className='w-3 h-3' src={rateIcon} alt="" />
-                    <p className='text-[#00D390] font-medium ml-2'>5</p>
+                    <p className='text-[#00D390] font-medium ml-2'>{reviews}</p>
                 </button>
             </div>
         </div>
